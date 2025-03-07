8fa8fe50d91ca969eba74f31c135ea2bb1589d9d
RAidanM <r.aidan.mealey@gmail.com>
2025-03-07 17:01:40 -0500

    Initial commit

---
+++ initializeDatabase.py
@@ -0,0 +1,32 @@
+import sqlite3
+
+conn = sqlite3.connect('scheduler.db')
+print("database opened")
+
+conn.execute(
+    '''
+    CREATE TABLE LIBRARY (
+        SLIDE_ID        INT PRIMARY KEY     NOT NULL,
+        SLIDE_NAME      TEXT                NOT NULL,
+        FILE_LOCATION   TEXT                NOT NULL,
+        MUST_FOLLOW     INT,
+        MUST_PRECEDE    INT,
+        MUST_AVOID      TEXT
+    );
+    '''
+)
+
+conn.execute(
+    '''
+    CREATE TABLE SCHEDULE (
+        SCHEDULE_ID     INT PRIMARY KEY     NOT NULL,
+        SLIDE_ID        INT                 NOT NULL,
+        START_DATE      TEXT                NOT NULL,
+        END_DATE        TEXT,
+        DAY             TEXT,
+        FOREIGN KEY (SLIDE_ID) REFERENCES LIBRARY(SLIDE_ID) ON UPDATE CASCADE ON DELETE CASCADE
+    );
+    '''
+)
+
+conn.close
